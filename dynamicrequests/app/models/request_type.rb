5dbf484ee05dc502b760424c71f19ac78d857c25
AlexUkPC <alexrogna@yahoo.com>
2022-04-01 10:51:22 +0200

    has many

--- dynamicrequests/app/models/request_type.rb
+++ dynamicrequests/app/models/request_type.rb
@@ -1,4 +1,5 @@
 class RequestType < ApplicationRecord
   has_many :fields, class_name: "RequestField", dependent: :destroy
 	accepts_nested_attributes_for :fields, reject_if: :all_blank, allow_destroy: true
+  has_many :requests, dependent: :destroy
 end
